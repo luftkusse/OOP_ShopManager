45326234b5fa5fdd2b658cec9aac1dff6f8c6388
luftkusse <luftkusse@gmail.com>
2023-01-16 09:34:53 +0300

    main commit

---
+++ UnitTestShopManager/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestShopManager")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestShopManager")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("68d33659-921d-4e51-97f2-b365f853fe9e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestShopManager/UnitTest.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ShopManager.Models;
+
+namespace UnitTestShopManager
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void TestMethodCategories()
+        {
+            Assert.AreEqual(Categories.Food, Categories.Clothes - 1);
+            Assert.AreEqual(Categories.Clothes, Categories.Electronics - 1);
+        }
+
+        [TestMethod]
+        public void TestMethodProduct()
+        {
+            Product product = new Product();
+            product.Name = "Tom";
+            Assert.AreEqual(product.Name, "Tom");
+            product.Description = "...";
+            Assert.AreEqual(product.Description, "...");
+            product.Price = 10;
+            Assert.AreEqual(product.Price, 10);
+            product.Category = Categories.Food;
+            Assert.AreEqual(product.Category, Categories.Food);
+        }
+    }
+}
